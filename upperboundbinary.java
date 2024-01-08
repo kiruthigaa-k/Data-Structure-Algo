9d4add770a84dbb2bfc763cf25ef209d02a93cf9
Kiruthigaa <112771633+kiruthigaa-k@users.noreply.github.com>
2024-01-08 09:02:13 +0530

    Update upperboundbinary.java

--- upperboundbinary.java
+++ upperboundbinary.java
@@ -11,7 +11,7 @@ public class Solution {
                 right =mid-1;
             }
             else{
-                right=mid+1;
+                left=mid+1;
             }
         }
         return ans;
